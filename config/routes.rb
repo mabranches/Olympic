413ab694ddca7948c246a6f2158805e6183dd47a
Manoel Rebelo Abranches <abranches@gmail.com>
2016-06-25 23:44:15 -0300

    rotas para cada tipo de competicao

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   jsonapi_resources :competitions, except: [:new, :edit] do
     get :types, on: :collection
   end
+
+  jsonapi_resources :run100ms
+  jsonapi_resources :javelin_throws
+
 end
