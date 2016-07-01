015db477aa362b8d2f332bef1977268155c5427f
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-01 02:18:27 -0300

    helper method for api controllers

---
+++ lib/controller_json_helper.rb
@@ -0,0 +1,24 @@
+module ControllerJsonHelper
+  private
+    def controller_json_helper(resource, query_result)
+      paginator = Paginator.new(params, base_url: base_url, path: path)
+      links = paginator.pagination_links(query_result.length)
+      query_result = paginator.paginate(query_result)
+
+      serializer = Serializer.new(resource, base_url: base_url)
+      serialized_data = query_result.collect do |result_item|
+        serializer.serialize(result_item)
+      end
+      {
+        data: serialized_data,
+        links: links
+      }
+    end
+    def base_url
+      @base_url ||= request.env['REQUEST_URI'].split(request.env['PATH_INFO'])[0]
+    end
+
+    def path
+      @path ||= request.env['PATH_INFO']
+    end
+end
