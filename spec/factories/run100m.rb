408f7b6bbf2efe2a82bb8c5727c803d13b17f911
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-01 02:34:21 -0300

    Correct Run100m factory

--- spec/factories/run100m.rb
+++ spec/factories/run100m.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :run100m do
     sequence(:name) {|n| "competition#{n}"}
-    type {CompetitionType.all.sample.name}
     status {[:running, :finished].sample}
   end
 end
