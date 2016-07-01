e17a096573d2bbc600401d5b2910d3eb8c4fe22d
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-01 02:17:15 -0300

    rank test in a shared example

--- spec/controllers/v1/competitions_controller_spec.rb
+++ spec/controllers/v1/competitions_controller_spec.rb
@@ -10,6 +10,55 @@ require 'rails_helper'
 #atletas
 #GET    /v1/athletes/:athlete_id/competitions(.:format)                            v1/competitions#index
 #GET    /v1/athletes/:athlete_id/competitions/:id(.:format)                        v1/competitions#show
+
+  def run100m_rank(score_values)
+    score_values.collect do |values|
+      values.min_by{|s| s[:value]}
+    end.sort do |a,b|
+      [a[:value] ,b[:athlete].age, a[:athlete].name] <=>
+        [b[:value], a[:athlete].age, b[:athlete].name]
+    end
+  end
+  def javalin_throw_rank(score_values)
+    score_values.collect do |values|
+      values.max_by{|s| s[:value]}
+    end.sort do |a,b|
+      [b[:value] ,b[:athlete].age, a[:athlete].name] <=>
+        [a[:value], a[:athlete].age, b[:athlete].name]
+    end
+  end
+
+  RSpec.shared_examples "competition rank" do |competition_type, n_athletes,
+    score_by_athlete, status, rank_scores_method|
+    it 'should show ranking in correct order' do
+      competition = create(competition_type, status: :running)
+      athletes = []
+      n_athletes.times {athletes << create(:athlete)}
+
+      #3 random score values for each athlete
+      score_values = []
+      n_athletes.times.each_with_index do |i|
+        score_values << []
+        score_by_athlete.times do
+          score = create(:score, competition: competition, athlete: athletes[i])
+          score_values[i] << {value: score.value, athlete: score.athlete}
+        end
+      end
+
+      expected_ranking = send(rank_scores_method, score_values)
+      competition.update_attributes!(status: status)
+
+      get :rank, competition_id: competition.id
+      expect(response).to have_http_status(:ok)
+      result = json_response
+      result[:data].each_with_index do |score, i|
+        expect(score[:attributes][:result]).to eq(expected_ranking[i][:value].to_f.to_s  + competition.unity)
+        expect(score[:attributes][:'athlete-name']).to eq(expected_ranking[i][:athlete].name)
+      end
+    end
+  end
+
+
 RSpec.describe V1::CompetitionsController, type: :controller do
 
   let(:json_response){JSON.parse(response.body, symbolize_names: true)}
@@ -95,40 +144,23 @@ RSpec.describe V1::CompetitionsController, type: :controller do
     end
   end
 
+  RSpec.shared_examples "a measurable object" do |measurement, measurement_methods|
+  measurement_methods.each do |measurement_method|
+    it "should return #{measurement} from ##{measurement_method}" do
+      expect(subject.send(measurement_method)).to eq(measurement)
+    end
+  end
+end
+
+
+
+
   describe 'GET #rank' do
     context 'javelin-throw competition' do
-      before do
-        @competition = create(:javelin_throw)
-        athletes = []
-        4.times {athletes << create(:athlete)}
-
-        #3 random score values for each athlete
-        score_values = []
-        4.times.each_with_index do |i|
-          score_values <<  []
-          3.times do
-            value = rand(100)
-            score_values[i] << {value: value, athlete: athletes[i]}
-            create(:score, competition: @competition, athlete: athletes[i])
-          end
-        end
-
-        #expected ranking output
-        @expected_ranking =  score_values.collect do |values|
-          values.max_by{|v| v[:value]}
-        end
-      end
-      it 'should show ranking in correct order' do
-        get :rank, id: @competition.id
-        expect(response).to have_http_status(:ok)
-        response = json_response
-        response[:data].each_with_index do |i|
-          expect(data[:attributes][:result]).to eq(@expected_ranking[i][:value]  + competition.unity)
-          expect(data[:attributes][:athlete_name]).to eq(@expected_ranking[i][:athlete].name)
-        end
-      end
+      it_should_behave_like 'competition rank', :javelin_throw, 40, 3, :running, :javalin_throw_rank
     end
     context 'run100m competition' do
+      it_should_behave_like 'competition rank', :run100m, 40, 1, :running, :run100m_rank
     end
     context 'running javelin-throw competition' do
     end
