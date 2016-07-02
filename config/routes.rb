c4ee22f017be2fbc5e1932d4220466ce6b4fe6d3
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-02 01:42:59 -0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     jsonapi_resources :competitions  do
-      jsonapi_resources :scores , only: [:index, :show] do
-      end
-      jsonapi_resources :athletes, only: [:index, :show] do
-        jsonapi_resources :scores do
+      jsonapi_relationships only: [:index]
+      jsonapi_resources :athletes, only: [:index] do
+        jsonapi_resources :scores, only: [:index, :create, :update] do
         end
       end
       get :rank
@@ -13,10 +12,7 @@ Rails.application.routes.draw do
     end
 
     jsonapi_resources :athletes do
-      jsonapi_resources :scores, only: [:index, :show] do
-      end
-      jsonapi_resources :competitions, only: [:index, :show] do
-      end
+      jsonapi_relationships only: [:index]
     end
 
     jsonapi_resources :scores, only: [:index, :show] do
