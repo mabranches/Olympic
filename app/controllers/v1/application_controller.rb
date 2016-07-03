05cd887bf7cca912547ae071acb014f5af4e2fa8
Manoel Rebelo Abranches <abranches@gmail.com>
2016-07-03 03:05:08 -0300

    Treat all excpetion with error logs and API responses

--- app/controllers/v1/application_controller.rb
+++ app/controllers/v1/application_controller.rb
@@ -1,6 +1,11 @@
 module V1
   class ApplicationController < ActionController::API
+    rescue_from Exception, with: :log_exception
     include ControllerJsonHelper
     include JSONAPI::ActsAsResourceController
+    def log_exception(e)
+      logger.error "#{e.message} - #{e.backtrace.inspect}"
+      render json: { errors:[{title: e.class.name.underscore, detail: e.message}, status: :internal_server_error] }
+    end
   end
 end
